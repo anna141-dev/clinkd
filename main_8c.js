8819268950cbad5e3312d703d28c907a45ca9647
anna141-dev <anna141-dev@users.noreply.github.com>
2026-06-09 10:38:19 +0000

    deploy: 041dbc43225214737bc7eb067decce42e74119bf

--- main_8c.js
+++ main_8c.js
@@ -7,5 +7,6 @@ var main_8c =
     [ "SECTION", "main_8c.html#aa2c44298bc422970bbd81c5622bce3ea", null ],
     [ "TEST", "main_8c.html#a4f22cf6b02b46dca510adb049eec0431", null ],
     [ "IsEven", "main_8c.html#ad3d999af6dccc2f1838a01650de3e285", null ],
+    [ "IsOdd", "main_8c.html#a3f082c751fb1c0d3c82dead08bae8d86", null ],
     [ "main", "main_8c.html#a840291bc02cba5474a4cb46a9b9566fe", null ]
 ];
--- search/functions_0.js
+++ search/functions_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['iseven_0',['IsEven',['../main_8c.html#ad3d999af6dccc2f1838a01650de3e285',1,'main.c']]]
+  ['iseven_0',['IsEven',['../main_8c.html#ad3d999af6dccc2f1838a01650de3e285',1,'main.c']]],
+  ['isodd_1',['IsOdd',['../main_8c.html#a3f082c751fb1c0d3c82dead08bae8d86',1,'main.c']]]
 ];
