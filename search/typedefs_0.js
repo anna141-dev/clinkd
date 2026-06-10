e25847ac864e824828a55bb1837859a418935c85
anna141-dev <anna141-dev@users.noreply.github.com>
2026-06-10 00:39:04 +0000

    deploy: 5afeebe6decbfd3d77aa1f9146b014db3965fa02

--- search/typedefs_0.js
+++ search/typedefs_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['singlylinkedlist_0',['SinglyLinkedList',['../singly__linked__list_8h.html#a96e3d8e2bd9d77a2724df394ad2f05fb',1,'singly_linked_list.h']]],
-  ['sslitem_1',['SSLItem',['../singly__linked__list_8h.html#a6fe773e040ef48af899cf6c89a9d2a9c',1,'singly_linked_list.h']]]
+  ['sllitem_1',['SLLItem',['../singly__linked__list_8h.html#aee94df893e28446d17db1b68bbc8c987',1,'singly_linked_list.h']]]
 ];
--- singly__linked__list_8c.js
+++ singly__linked__list_8c.js
@@ -1,17 +1,17 @@
 var singly__linked__list_8c =
 [
-    [ "SLLAppend", "singly__linked__list_8c.html#ab99c3599e8a491140adc930e1055a6dc", null ],
-    [ "SLLCountNodes", "singly__linked__list_8c.html#a4d667444cb07c3390112e05525d8180d", null ],
-    [ "SLLCreateSinglyLinkedList", "singly__linked__list_8c.html#a3027e1e11ec55e1f39c6469b2039fe8e", null ],
-    [ "SLLDeleteAt", "singly__linked__list_8c.html#ac79faaaf74f1e68d6099eda37019badc", null ],
-    [ "SLLFilter", "singly__linked__list_8c.html#ab2a1a0d9534d9081091081d10f516239", null ],
-    [ "SLLFind", "singly__linked__list_8c.html#a4bdab13c9fc7c84d6b1b3bf1e4546fac", null ],
-    [ "SLLFree", "singly__linked__list_8c.html#ae6d8802f26ccde96549248567e9c30fc", null ],
-    [ "SLLInsertAt", "singly__linked__list_8c.html#a2ee3c487d01462c7b67b9d7b8660a72b", null ],
-    [ "SLLMap", "singly__linked__list_8c.html#a5c6f639048d75c809ffb3c6b7fd7f51c", null ],
-    [ "SLLNodes", "singly__linked__list_8c.html#a1429ebbdcaa78010d93b9f98678fe0dd", null ],
-    [ "SLLPopBack", "singly__linked__list_8c.html#a88ce340f4efc3e5bb035542050704e5c", null ],
-    [ "SLLPopFront", "singly__linked__list_8c.html#ab62ded5bb5102a38b2908e5535d741f4", null ],
-    [ "SLLPropend", "singly__linked__list_8c.html#a84039b7c02271745d596b59403088f97", null ],
-    [ "SLLReverse", "singly__linked__list_8c.html#aec4ff18a299254378eb2ff269bef6c0c", null ]
+    [ "SLLAppend", "singly__linked__list_8c.html#ae09caed5ec1fe6632a2643014a6fa1a9", null ],
+    [ "SLLCountNodes", "singly__linked__list_8c.html#a3c19c9c9e098bedd6384df410651a2e9", null ],
+    [ "SLLCreate", "singly__linked__list_8c.html#afe8847022f040dff6d001d060177179d", null ],
+    [ "SLLDeleteAt", "singly__linked__list_8c.html#a6ed3ee36c38be39d362d64b4012d39bf", null ],
+    [ "SLLFilter", "singly__linked__list_8c.html#ad7dcc7b52b7e342f383666ba67c2eb0a", null ],
+    [ "SLLFind", "singly__linked__list_8c.html#a819914fe86f35c560ba6c1d65e0e1bdc", null ],
+    [ "SLLFree", "singly__linked__list_8c.html#a13e7e4b433862f810b45625467e79f66", null ],
+    [ "SLLInsertAt", "singly__linked__list_8c.html#a82b14a2769adc6071c184dd259949e6b", null ],
+    [ "SLLMap", "singly__linked__list_8c.html#af6a3b34b50471462ecb64b8b1212a003", null ],
+    [ "SLLNodes", "singly__linked__list_8c.html#a4b03643f597315fe33e9303928526b81", null ],
+    [ "SLLPopBack", "singly__linked__list_8c.html#aea879240b4125ca4cf78b3f0ac0d87b5", null ],
+    [ "SLLPopFront", "singly__linked__list_8c.html#a11fc915b4ac19794b360fb9555b64afb", null ],
+    [ "SLLPropend", "singly__linked__list_8c.html#a8ff25ed81ae8d5c76896f9621fd002ce", null ],
+    [ "SLLReverse", "singly__linked__list_8c.html#abda938b3a8b90d517c768e21540a7fd1", null ]
 ];
