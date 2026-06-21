3181d956797f0c33bd00f4baf37bb836a94a8441
anna141-dev <anna141-dev@users.noreply.github.com>
2026-06-21 23:03:46 +0000

    deploy: f1cbc81b2e63caacdb3a4732a5d1c3d85c90f633

--- doubly__linked__list_8h.js
+++ doubly__linked__list_8h.js
@@ -4,5 +4,6 @@ var doubly__linked__list_8h =
     [ "DLLNode", "doubly__linked__list_8h.html#ac6f2730837a0c45f7f337cf76c4aabb2", null ],
     [ "DLLAppend", "doubly__linked__list_8h.html#a563f330fb4299d21ff4e9d73c4afeb85", null ],
     [ "DLLCountNodes", "doubly__linked__list_8h.html#a8c40cdd675d5d87b64f8744d3513841e", null ],
-    [ "DLLNodes", "doubly__linked__list_8h.html#a03f46ea9b5a0f29e8ab8bfc8a55ecdb0", null ]
+    [ "DLLNodes", "doubly__linked__list_8h.html#a03f46ea9b5a0f29e8ab8bfc8a55ecdb0", null ],
+    [ "DLLPrepend", "doubly__linked__list_8h.html#af6b6367a4b44d30691c1d80f57bd1e49", null ]
 ];
--- search/all_3.js
+++ search/all_3.js
@@ -5,7 +5,8 @@ var searchData=
   ['dllcountnodes_2',['dllcountnodes',['../doubly__linked__list_8h.html#a8c40cdd675d5d87b64f8744d3513841e',1,'DLLCountNodes(DLLNode *head):&#160;doubly_linked_list.c'],['../doubly__linked__list_8c.html#a8c40cdd675d5d87b64f8744d3513841e',1,'DLLCountNodes(DLLNode *head):&#160;doubly_linked_list.c']]],
   ['dllnode_3',['dllnode',['../structDLLNode.html',1,'DLLNode'],['../doubly__linked__list_8h.html#ac6f2730837a0c45f7f337cf76c4aabb2',1,'DLLNode:&#160;doubly_linked_list.h']]],
   ['dllnodes_4',['dllnodes',['../doubly__linked__list_8h.html#a03f46ea9b5a0f29e8ab8bfc8a55ecdb0',1,'DLLNodes(DLLNode *head, void(*print_node)(DLLNode *)):&#160;doubly_linked_list.c'],['../doubly__linked__list_8c.html#a03f46ea9b5a0f29e8ab8bfc8a55ecdb0',1,'DLLNodes(DLLNode *head, void(*print_node)(DLLNode *)):&#160;doubly_linked_list.c']]],
-  ['documentation_5',['Documentation',['../md_README.html#autotoc_md5',1,'']]],
-  ['doubly_5flinked_5flist_2ec_6',['doubly_linked_list.c',['../doubly__linked__list_8c.html',1,'']]],
-  ['doubly_5flinked_5flist_2eh_7',['doubly_linked_list.h',['../doubly__linked__list_8h.html',1,'']]]
+  ['dllprepend_5',['dllprepend',['../doubly__linked__list_8h.html#af6b6367a4b44d30691c1d80f57bd1e49',1,'DLLPrepend(DLLNode **head, DLLNode *node):&#160;doubly_linked_list.c'],['../doubly__linked__list_8c.html#af6b6367a4b44d30691c1d80f57bd1e49',1,'DLLPrepend(DLLNode **head, DLLNode *node):&#160;doubly_linked_list.c']]],
+  ['documentation_6',['Documentation',['../md_README.html#autotoc_md5',1,'']]],
+  ['doubly_5flinked_5flist_2ec_7',['doubly_linked_list.c',['../doubly__linked__list_8c.html',1,'']]],
+  ['doubly_5flinked_5flist_2eh_8',['doubly_linked_list.h',['../doubly__linked__list_8h.html',1,'']]]
 ];
