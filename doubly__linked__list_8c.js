c3c9ea686c2969e42b451c1482dfc4683cf78664
anna141-dev <anna141-dev@users.noreply.github.com>
2026-06-22 22:58:54 +0000

    deploy: abc474bf3916aaa62729db62863abc62ec475b0c

--- doubly__linked__list_8c.js
+++ doubly__linked__list_8c.js
@@ -2,6 +2,8 @@ var doubly__linked__list_8c =
 [
     [ "DLLAppend", "doubly__linked__list_8c.html#a563f330fb4299d21ff4e9d73c4afeb85", null ],
     [ "DLLCountNodes", "doubly__linked__list_8c.html#a8c40cdd675d5d87b64f8744d3513841e", null ],
+    [ "DLLDeleteAt", "doubly__linked__list_8c.html#a2d4eefc1fb73c49972234f9e4c1b2f96", null ],
+    [ "DLLInsertAt", "doubly__linked__list_8c.html#a78fd58f5a04cc33bd962ca307db66fe3", null ],
     [ "DLLNodes", "doubly__linked__list_8c.html#a03f46ea9b5a0f29e8ab8bfc8a55ecdb0", null ],
     [ "DLLPopBack", "doubly__linked__list_8c.html#ab92a9d7dba0545fe35669f87b1382d8e", null ],
     [ "DLLPopFront", "doubly__linked__list_8c.html#a124d5cfd9f1ec9561e254286462f2521", null ],
--- doubly__linked__list_8h.js
+++ doubly__linked__list_8h.js
@@ -4,6 +4,8 @@ var doubly__linked__list_8h =
     [ "DLLNode", "doubly__linked__list_8h.html#ac6f2730837a0c45f7f337cf76c4aabb2", null ],
     [ "DLLAppend", "doubly__linked__list_8h.html#a563f330fb4299d21ff4e9d73c4afeb85", null ],
     [ "DLLCountNodes", "doubly__linked__list_8h.html#a8c40cdd675d5d87b64f8744d3513841e", null ],
+    [ "DLLDeleteAt", "doubly__linked__list_8h.html#a2d4eefc1fb73c49972234f9e4c1b2f96", null ],
+    [ "DLLInsertAt", "doubly__linked__list_8h.html#a78fd58f5a04cc33bd962ca307db66fe3", null ],
     [ "DLLNodes", "doubly__linked__list_8h.html#a03f46ea9b5a0f29e8ab8bfc8a55ecdb0", null ],
     [ "DLLPopBack", "doubly__linked__list_8h.html#ab92a9d7dba0545fe35669f87b1382d8e", null ],
     [ "DLLPopFront", "doubly__linked__list_8h.html#a124d5cfd9f1ec9561e254286462f2521", null ],
